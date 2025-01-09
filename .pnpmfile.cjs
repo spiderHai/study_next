fa190515336b4d9fb92e3ca9fd990f2d14986958
lyh <1710997049@qq.com>
2025-01-09 15:35:50 +0800

    init

--- .pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    hooks: {
-      readPackage(pkg) {
-        pkg.publishConfig = pkg.publishConfig || {};
-        pkg.publishConfig.registry = 'https://registry.npmjs.org/';
-        return pkg;
-      }
-    }
-  };
